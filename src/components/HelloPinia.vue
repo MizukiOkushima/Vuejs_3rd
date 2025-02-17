6ae3dd86f646df2e6672da3666658d2592621e3d
MizukiOkushima <m.okushima@fukuinet.co.jp>
2025-02-17 16:52:05 +0900

    second commit

---
+++ src/components/HelloPinia.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { useStoreCounter } from '../stores/counter';
+const counter = useStoreCounter();
+</script>
+
+<template>
+  <h2>Hello Pinia</h2>
+  <p>カウント:{{ counter.count }}</p>
+</template>
