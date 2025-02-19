762918fec0023f6edba12c8de793c02379b30ca6
MizukiOkushima <m.okushima@fukuinet.co.jp>
2025-02-19 12:24:03 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import { useStoreCounter } from './stores/counter';
 import HelloPinia from './components/HelloPinia.vue';
+import { storeToRefs } from 'pinia';
 const counter = useStoreCounter();
+const { count, doubleCount } = storeToRefs(counter);
+const { increment } = counter;
 </script>
 
 <template>
   <h1>Pinia入門</h1>
-  <p>Count: {{ counter.count }}</p>
+  <p>Count: {{ count }}</p>
+  <p>DoubleCount: {{ doubleCount }}</p>
+  <div>
+    <button @click="increment">up</button>
+  </div>
   <HelloPinia />
 </template>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,7 +1,29 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 
-export const useStoreCounter = defineStore('counter', {
-  state: () => ({
-    count: 1,
-  }),
+// export const useStoreCounter = defineStore("counter", {
+//   state: () => ({
+//     count: 1,
+//     count2: 2,
+//     user: {
+//       name: "John Doe",
+//     },
+//   }),
+//   getters: {
+//     doubleCount: (state) => state.count * 2,
+//   },
+//   actions: {
+//     increment() {
+//       this.count++;
+//     },
+//   },
+// });
+
+export const useStoreCounter = defineStore("counter", () => {
+  const count = ref(1);
+  const increment = () => {
+    count.value++;
+  };
+  const doubleCount = computed(() => count.value * 2);
+  return { count, increment, doubleCount };
 });
